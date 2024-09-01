292a1634b88d0902d4ad21f414c726dffffea23c
subrahmanyam <subrahmanyamkalla8@gmail.com>
2024-09-01 18:14:02 +0530

    added 22-redirectors

--- expense-mysql-shell.sh
+++ expense-mysql-shell.sh
@@ -44,7 +44,7 @@ VALIDATE $? "enabled MySQL server"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Started MySQL sever"
 
-mysql -h 44.204.35.167 -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+mysql -h 44.201.150.124 -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
 if [ $? -ne 0 ]
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
